dd492d59473e09ed0b849125da0a9e40bf8f9492
nickconradi <145594857+nickconradi@users.noreply.github.com>
2024-05-07 11:15:02 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -1,2 +1,14 @@
 INSERT INTO funcionarios (idFuncionarios,Salario,CPF,NomeFuncionario,Login,Senha) VALUES (?,?,?,?,?,?);
 
+--Carrinho  
+
+INSERT INTO Carrinho (Vendas_idVendas, Produtos_idProdutos, Quantidade)
+VALUES (?, ?, ?);
+
+
+--Cliente 
+
+INSERT INTO Clientes (Nome_Clientes, CPF, Email, Telefone);
+VALUES (?, ?, ?, ?);
+
+
