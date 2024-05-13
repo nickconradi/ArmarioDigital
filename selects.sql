5255e66ed6aac94c1a704a32ad5fb4a3983ce9f5
Letícia Gehrke Bruda <leticia.gb@aluno.ifsc.edu.br>
2024-05-13 08:55:10 -0300

    select..

--- selects.sql
+++ selects.sql
@@ -1,8 +1,9 @@
-<<<<<<< Updated upstream
+--Funcionarios
 SELECT * FROM funcionarios;
-=======
---Produtos
-SELECT * FROM Produtos;
->>>>>>> Stashed changes
 
 SELECT * FROM clientes WHERE CPF = ? AND Email = ?;
+
+--Produtos
+SELECT * FROM Produtos;
+
+
