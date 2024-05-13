dc09211f0fa6ec6b975475fbd9484986126cc21e
Letícia Gehrke Bruda <leticia.gb@aluno.ifsc.edu.br>
2024-05-13 10:29:22 -0300

    delete (produto)

--- deletes.sql
+++ deletes.sql
@@ -1,6 +1,5 @@
 --Funcionarios
---Java
-DELETE FROM funcionarios WHERE CPF LIKE ?;
---Normal
-DELETE FROM funcionarios WHERE CPF LIKE '317.523.170-04';
-SELECT * FROM funcionarios;
+DELETE FROM funcionarios WHERE id_funcionario LIKE ?;
+
+--Produtos
+DELETE FROM Produtos WHERE id_produto LIKE ?;
