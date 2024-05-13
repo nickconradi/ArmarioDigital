b914ce250a7b606dcb6fdbb7bf65d0def92cee7b
henrique <henrique.l19@aluno.ifsc.edu.br>
2024-05-13 11:10:52 -0300

    delete forn.

--- deletes.sql
+++ deletes.sql
@@ -3,3 +3,6 @@ DELETE FROM funcionarios WHERE id_funcionario LIKE ?;
 
 --Produtos
 DELETE FROM Produtos WHERE id_produto LIKE ?;
+
+--Fornecedores
+DELETE FROM Fornecedores WHERE id_Fornecedores LIKE ?;
