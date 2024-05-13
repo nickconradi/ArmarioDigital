75a935ad5e956c582c1a3b82a4181609436fac2b
Letícia Gehrke Bruda <leticia.gb@aluno.ifsc.edu.br>
2024-05-13 10:05:14 -0300

    select join produtos

--- selects.sql
+++ selects.sql
@@ -6,4 +6,4 @@ SELECT * FROM clientes WHERE CPF = ? AND Email = ?;
 --Produtos
 SELECT * FROM Produtos;
 
-
+SELECT * FROM Produtos AS p INNER JOIN Fornecedores AS f ON f.id_produto = p.id_produto WHERE p.id_produto = 12 ORDER BY p.id_produto;
