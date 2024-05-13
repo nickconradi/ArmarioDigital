83cad67394ca7416b784adbb6e901ce425688768
Isaque José <isaque.jp2007@aluno.ifsc.edu.br>
2024-05-13 10:57:46 -0300

    Select

--- selects.sql
+++ selects.sql
@@ -1,12 +1,16 @@
---Funcionarios
+--Select all
 SELECT * FROM funcionarios;
-
-SELECT * FROM clientes WHERE CPF = ? AND Email = ?;
-
---Produtos
 SELECT * FROM Produtos;
-
---Fornecedores
 SELECT * FROM Fornecedores; 
 
+
+--Select where
+SELECT * FROM clientes WHERE CPF = ? AND Email = ?;
+
+--Select Join
+select * from vendas as v
+inner join clientes as c
+on c.idCliente = v.Cliente_idCliente1
+inner join funcionarios as f
+on f.CPF = v.Funcionarios_idFuncionario;
 
