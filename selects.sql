0f0952f16f87d1ca090cce0021e4db264875c8e4
henrique <henrique.l19@aluno.ifsc.edu.br>
2024-05-13 10:27:29 -0300

    change

--- selects.sql
+++ selects.sql
@@ -6,13 +6,7 @@ SELECT * FROM clientes WHERE CPF = ? AND Email = ?;
 --Produtos
 SELECT * FROM Produtos;
 
-<<<<<<< HEAD
 --Fornecedores
 SELECT * FROM Fornecedores; 
 
 
-=======
-SELECT * FROM Produtos AS p 
-INNER JOIN Fornecedores AS f ON f.id_produto = p.id_produto 
-ORDER BY p.id_produto;
->>>>>>> 118e5b716a7e4e5f93b48630122d84d2ca9f2dca
